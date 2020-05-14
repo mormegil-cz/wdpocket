da6b244bf0a16576df7be8db3432cd9d62539109
mormegil <mormegil@centrum.cz>
2020-05-14 19:59:18 +0200

    Application state, lazy entity loading

--- lib/main.dart
+++ lib/main.dart
@@ -3,70 +3,62 @@ import 'package:flutter/material.dart';
 import 'package:pocket_data/api.dart';
 
 import 'model.dart';
-import 'adams.dart';
+// import 'adams.dart';
+
+final entitySource = WikibaseApi();
 
 void main() async {
-  var adamsSample = Item(
-      qid: "Q42",
-      labels: {"en": "Douglas Adams"},
-      descriptions: {"en": "English writer"},
-      aliases: {},
-      claims: {
-        "P31": <Claim>[
-          Claim(
-              id: r"Q42$F078E5B3-F9A8-480E-B7AC-D97778CBBEF9",
-              rank: ClaimRank.normal,
-              mainSnak: ValueSnak(dataType: "wikibase-item", value: EntityIdValue("Q5"), hash: "ad7d38a03cdd40cdc373de0dc4e7b7fcbccb31d9"),
-              qualifiers: LinkedHashMap.from({}),
-              references: [])
-        ]
-      },
-      siteLinks: {"enwiki": SiteLink(pageTitle: "Douglas Adams")},
-      lastRevId: "0",
-      modified: DateTime(2020, 05, 13, 10, 22, 31));
-
-  //var adams = Entity.fromParsedJson(q42ParsedJson);
-
-  var entitySource = WikibaseApi();
-  var entity = await entitySource.getEntity("Q256");
-
-  runApp(MyApp(entity: entity));
+  runApp(WdPocketApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({@required this.entity});
+class WdPocketApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => WdPocketAppState();
+}
 
-  final String title = "WD Pocket";
-  final Entity entity;
+class WdPocketAppState extends State<WdPocketApp> {
+  String _qid;
+  Entity _entity;
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: this.title,
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-          // This makes the visual density adapt to the platform that you run
-          // the app on. For desktop platforms, the controls will be smaller and
-          // closer together (more dense) than on mobile platforms.
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text(this.title),
-            actions: <Widget>[Icon(Icons.search)],
-          ),
-          body: EntityView(entity: this.entity),
-        ));
+  void initState() {
+    super.initState();
+    requestLoadEntity("Q42");
   }
+
+  void requestLoadEntity(String qid) {
+    entitySource.getEntity(qid).then((loadedEntity) => this.setState(() {
+      _qid = qid;
+      _entity = loadedEntity;
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+      title: this._qid ?? "",
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(this._qid ?? ""),
+          actions: <Widget>[IconButton(icon: Icon(Icons.search))],
+        ),
+        body: _entity == null ? Container() : EntityView(entity: _entity),
+      ));
 }
 
 class EntityView extends StatelessWidget {
