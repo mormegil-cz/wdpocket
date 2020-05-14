6097ddaaed458db7c63291b11abf3ca55e0fab46
mormegil <mormegil@centrum.cz>
2020-05-14 13:23:45 +0200

    View of all kinds of data

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ void main() async {
   var entitySource = WikibaseApi();
   var entity = await entitySource.getEntity("Q256");
 
-  runApp(MyApp(entity:entity));
+  runApp(MyApp(entity: entity));
 }
 
 class MyApp extends StatelessWidget {
@@ -61,41 +61,114 @@ class MyApp extends StatelessWidget {
         ),
         home: Scaffold(
           appBar: AppBar(
-            // Here we take the value from the MyHomePage object that was created by
-            // the App.build method, and use it to set our appbar title.
             title: Text(this.title),
+            actions: <Widget>[Icon(Icons.search)],
           ),
-          body: EntityView(
-              entity: this.entity),
+          body: EntityView(entity: this.entity),
         ));
   }
 }
 
 class EntityView extends StatelessWidget {
-  EntityView({@required this.entity, Key key})
-      : orderedClaims = entity.claims.entries.toList(),
+  EntityView({@required this.entity, Key key}) : super(key: key);
+
+  final Entity entity;
+
+  @override
+  Widget build(BuildContext context) => DefaultTabController(
+        length: entity.type == EntityType.item ? 4 : 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.label_outline)),
+                Tab(icon: Icon(Icons.library_books)),
+                Tab(icon: Icon(Icons.perm_identity)),
+                if (entity.type == EntityType.item) Tab(icon: Icon(Icons.link))
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              EntityLabellingView(entity: entity),
+              EntityClaimView(orderedClaims: _getClaims((dataType) => dataType != "external-id")),
+              EntityClaimView(orderedClaims: _getClaims((dataType) => dataType == "external-id")),
+              if (entity.type == EntityType.item) EntitySiteLinksView(orderedLinks: (entity as Item).siteLinks.entries.toList())
+            ],
+          ),
+        ),
+      );
+
+  List<MapEntry<String, List<Claim>>> _getClaims(bool dataTypeFilter(String dataType)) {
+    // TODO: This filters on actual values, it should rather filter on property definitions, since properties with no value snaks appear everywhere right now
+    return entity.claims.entries
+        .where((claim) => !claim.value
+            .where((claimValue) => claimValue.mainSnak is ValueSnak)
+            .every((claimValue) => !dataTypeFilter((claimValue.mainSnak as ValueSnak).dataType)))
+        .toList();
+  }
+}
+
+class EntityLabellingView extends StatelessWidget {
+  EntityLabellingView({@required Entity entity, Key key})
+      : this.entity = entity,
+        this._labellingLanguages =
+            Set<String>.from(entity.labels.keys).union(Set<String>.from(entity.descriptions.keys)).union(Set<String>.from(entity.aliases.keys)).toList(),
         super(key: key);
 
   final Entity entity;
+  final List<String> _labellingLanguages;
+
+  @override
+  Widget build(BuildContext context) => ListView.builder(
+      itemCount: _labellingLanguages.length,
+      itemBuilder: (content, index) => Container(
+          decoration: BoxDecoration(border: Border(top: BorderSide())),
+          padding: EdgeInsets.symmetric(vertical: 5),
+          child: _buildLanguageWidget(context, _labellingLanguages[index])));
+
+  Widget _buildLanguageWidget(BuildContext context, String language) => Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: <Widget>[
+        Text(language, style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.3, fontWeightDelta: 3)),
+        Container(
+            padding: EdgeInsets.only(left: 5),
+            child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+              Text(entity.labels[language] ?? "", style: TextStyle(fontWeight: FontWeight.bold)),
+              Text(entity.descriptions[language] ?? "", style: TextStyle(fontStyle: FontStyle.italic)),
+              Text(entity.aliases[language]?.join("|") ?? ""),
+            ]))
+      ]);
+}
+
+class EntityClaimView extends StatelessWidget {
+  const EntityClaimView({@required this.orderedClaims, Key key}) : super(key: key);
+
   final List<MapEntry<String, List<Claim>>> orderedClaims;
 
   @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: entity.claims.length,
-        itemBuilder: (context, index) =>
-            Container(
-                decoration: BoxDecoration(border: Border(top: BorderSide())),
-                padding: EdgeInsets.symmetric(vertical: 5),
-                child: _buildClaimWidget(context, orderedClaims[index].key, orderedClaims[index].value)));
-  }
+  Widget build(BuildContext context) => ListView.builder(
+      itemCount: orderedClaims.length,
+      itemBuilder: (context, index) => Container(
+          decoration: BoxDecoration(border: Border(top: BorderSide())),
+          padding: EdgeInsets.symmetric(vertical: 5),
+          child: _buildClaimWidget(context, orderedClaims[index].key, orderedClaims[index].value)));
 
-  Widget _buildClaimWidget(BuildContext context, String propertyId, List<Claim> claims) {
+  static Widget _buildClaimWidget(BuildContext context, String propertyId, List<Claim> claims) {
     final List<Widget> claimWidgets = claims.map((claim) => ListTile(key: ValueKey(claim.id), title: Text(claim.toString()))).toList(growable: false);
 
     return Column(children: <Widget>[
-      Text(propertyId, style: TextStyle(fontWeight: FontWeight.bold)),
+      Text(propertyId, style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.3, fontWeightDelta: 3)),
       Container(padding: EdgeInsets.only(left: 5), child: Column(children: claimWidgets))
     ]);
   }
+}
+
+class EntitySiteLinksView extends StatelessWidget {
+  const EntitySiteLinksView({@required this.orderedLinks, Key key}) : super(key: key);
+
+  final List<MapEntry<String, SiteLink>> orderedLinks;
+
+  @override
+  Widget build(BuildContext context) => ListView.builder(
+      itemBuilder: (content, index) => ListTile(leading: Text(orderedLinks[index].key), title: Text(orderedLinks[index].value.pageTitle)),
+      itemCount: orderedLinks.length);
 }
